e894a389779b11e1b5bf77323c9af255ece4f00d
arkii1 <tommy.mapp@hotmail.com>
2022-11-15 16:13:32 +0000

    Add PinnedList

---
+++ client/src/components/common/PinnedList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import PinnedCard from './PinnedCard'
+
+function PinnedList({ title, dataArr }) {
+  return (
+    <div className="pinned-list flex flex-col justify-around px-3 pt-0 pb-3 gap-2">
+      <h3 className="text-lg font-medium text-gray-500	">{title}</h3>
+      <div className="flex justify-between">
+        {dataArr.map((data) => (
+          <PinnedCard
+            initials={data.initials}
+            name={data.name}
+            teamColour={data.teamColour}
+            link={data.link}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default PinnedList
+
+PinnedList.propTypes = {
+  title: propTypes.string.isRequired,
+  dataArr: propTypes.arrayOf(propTypes.shape).isRequired,
+}
