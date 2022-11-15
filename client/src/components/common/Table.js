8afc3b2e60e72f8d97da3b2ccb0665ba603b3056
arkii1 <tommy.mapp@hotmail.com>
2022-11-15 16:13:37 +0000

    Add Table

---
+++ client/src/components/common/Table.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import uniqid from 'uniqid'
+
+function Table({ headerArr, dataArr }) {
+  const headers = headerArr.map((h) => <th key={uniqid()}>{h}</th>)
+
+  const rows = dataArr.map((data) => (
+    <tr key={uniqid()}>
+      {data.map((d) => (
+        <td key={uniqid()}>{d}</td>
+      ))}
+    </tr>
+  ))
+  return (
+    <div className="overflow-x-auto p-3">
+      <table className="table table-zebra w-full">
+        <thead>
+          <tr>{headers}</tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Table
+
+Table.propTypes = {
+  headerArr: propTypes.arrayOf(propTypes.string).isRequired,
+  dataArr: propTypes.arrayOf(propTypes.arrayOf(propTypes.string)).isRequired,
+}
