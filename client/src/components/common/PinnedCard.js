7b0a563da5d2261b7423a75d2bcc4d93ac114ff6
arkii1 <tommy.mapp@hotmail.com>
2022-11-15 16:13:25 +0000

    Add PinnedCard

---
+++ client/src/components/common/PinnedCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import propTypes from 'prop-types'
+import './styles.css'
+
+function PinnedCard({ initials, name, teamColour, link }) {
+  return (
+    <a
+      key={initials}
+      href={link}
+      className="flex rounded-md border-solid border w-1/5 "
+    >
+      <span className={`project-card--${teamColour} rounded-l-md p-3`}>
+        {initials}
+      </span>
+      <h5 className="rounded-r-md p-3 truncate">{name}</h5>
+    </a>
+  )
+}
+
+export default PinnedCard
+
+PinnedCard.propTypes = {
+  initials: propTypes.string.isRequired,
+  name: propTypes.string.isRequired,
+  teamColour: propTypes.string.isRequired,
+  link: propTypes.string.isRequired,
+}
