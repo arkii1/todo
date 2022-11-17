378c66775306bb1692cd66fd83da02e80bbb62ba
arkii1 <tommy.mapp@hotmail.com>
2022-11-17 16:31:16 +0000

    Alter server script

--- server/app.js
+++ server/app.js
@@ -1,41 +1,27 @@
-const createError = require('http-errors')
 const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
-const logger = require('morgan')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
-const { default: mongoose } = require('mongoose')
 const indexRouter = require('./routes/index')
 
 const mongoDB = process.env.MONGO_URI
+
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const app = express()
+const port = 5000
 
-app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
+app.use(cors())
 
 app.use('/', indexRouter)
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404))
-})
-
-// error handler
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
-
-  // render the error page
-  res.status(err.status || 500)
-  res.render('error')
-})
+app.listen(port, () => `Server running on port ${port}`)
 
 module.exports = app
