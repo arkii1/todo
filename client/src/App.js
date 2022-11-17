ca1dc52aaba9c4e584ca66f7a7cb6fc055c2ead0
arkii1 <tommy.mapp@hotmail.com>
2022-11-17 10:48:04 +0000

    Add router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,109 @@
 import React from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Sidebar from './components/layout/Sidebar'
+import PinnedList from './components/common/PinnedList'
+import Table from './components/common/Table'
+import Footer from './components/layout/Footer'
 
 function App() {
-  return <div className="App">app</div>
+  return (
+    <div className="App min-h-screen">
+      <Sidebar />
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route
+              index
+              element={
+                <>
+                  <PinnedList
+                    title="Pinned Projects"
+                    dataArr={[
+                      {
+                        initials: 'TS',
+                        name: 'TEST',
+                        teamColour: 'yellow',
+                        link: 'test',
+                      },
+                      {
+                        initials: 'TS',
+                        name: 'TEST',
+                        teamColour: 'purple',
+                        link: 'test',
+                      },
+                      {
+                        initials: 'TS',
+                        name: 'TEST',
+                        teamColour: 'green',
+                        link: 'test',
+                      },
+                      {
+                        initials: 'TS',
+                        name: 'TEST',
+                        teamColour: 'pink',
+                        link: 'test',
+                      },
+                    ]}
+                  />
+                  <Table
+                    headerArr={['Project', 'Last Updated']}
+                    dataArr={[
+                      ['Placeholder', 'Yesterday'],
+                      ['Placeholder', 'Yesterday'],
+                      ['Placeholder', 'Yesterday'],
+                      ['Placeholder', 'Yesterday'],
+                    ]}
+                  />
+                  <div className="divider px-5" />
+                  <PinnedList
+                    title="Pinned Teams"
+                    dataArr={[
+                      {
+                        initials: 'TS',
+                        name: 'TEST',
+                        teamColour: 'yellow',
+                        link: 'test',
+                      },
+                      {
+                        initials: 'TS',
+                        name: 'TEST',
+                        teamColour: 'purple',
+                        link: 'test',
+                      },
+                      {
+                        initials: 'TS',
+                        name: 'TEST',
+                        teamColour: 'green',
+                        link: 'test',
+                      },
+                      {
+                        initials: 'TS',
+                        name: 'TEST',
+                        teamColour: 'pink',
+                        link: 'test',
+                      },
+                    ]}
+                  />
+                  <Table
+                    headerArr={['Team', 'Last Updated']}
+                    dataArr={[
+                      ['Placeholder', 'Yesterday'],
+                      ['Placeholder', 'Yesterday'],
+                      ['Placeholder', 'Yesterday'],
+                      ['Placeholder', 'Yesterday'],
+                    ]}
+                  />
+                </>
+              }
+            />
+          </Routes>
+        </BrowserRouter>
+      </main>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
