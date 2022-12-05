3de836016f944efa7708e2af1b2680f62eb60a30
arkii1 <tommy.mapp@hotmail.com>
2022-12-05 13:23:13 +0000

    Add Footer

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="footer p-10 bg-neutral text-neutral-content ">
+      <div>
+        <p>Created by Tommy Mapp</p>
+      </div>
+      <div>
+        <span className="footer-title">Social</span>
+        <div className="grid grid-flow-col gap-4" />
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
