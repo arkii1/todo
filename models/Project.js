92436ee18a451fd8cc8773ecbc81c00e85af79c7
arkii1 <tommy.mapp@hotmail.com>
2022-12-05 13:26:05 +0000

    Add Project

---
+++ models/Project.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const { Schema } = mongoose
+
+const ProjectSchema = new Schema({
+  name: { type: String, required: true },
+  slug: { type: String, required: true },
+  description: { type: String },
+  image: {
+    type: String,
+    default:
+      'https://img.freepik.com/premium-vector/hand-painted-background-violet-orange-colours_23-2148427578.jpg?w=2000',
+  },
+  created: { type: Date, required: true },
+  lastUpdated: { type: Date, required: true },
+  createdUser: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  contributors: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+})
+
+module.exports = mongoose.model('Project', ProjectSchema)
