493ae4e21bfb2e1252ab8b84f9f34de34ef54845
arkii1 <tommy.mapp@hotmail.com>
2022-12-06 09:33:31 +0000

    Redo server so it works, involved moving a bunch of files, installs and creating new server.js instead of app.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index')
 require('dotenv').config()
@@ -12,6 +12,7 @@ const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const app = express()
+const port = process.env.PORT || '5000'
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -22,5 +23,6 @@ app.use((req, res, next) => {
 app.use(cors())
 
 app.use('/', indexRouter)
+app.listen(port, () => `Server running on port ${port}`)
 
 module.exports = app
