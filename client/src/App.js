b308b90630181c49c982b8232f0bd28d2fb93845
arkii1 <tommy.mapp@hotmail.com>
2022-12-12 10:59:43 +0000

    Implement new Modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
@@ -9,14 +8,12 @@ import Table from './components/common/Table'
 import Footer from './components/layout/Footer'
 import Header from './components/layout/Header'
 import Checkbox from './components/common/Checkbox'
-import CreateTaskModal from './components/common/CreateTaskModal'
+import Modal from './components/common/Modal'
 import Details from './components/common/Details'
 
 import { ProjectProvider } from './contexts/ProjectContext'
 
 function App() {
-  // Context
-
   return (
     <div className="App min-h-screen">
       <ProjectProvider>
@@ -114,7 +111,55 @@ function App() {
                 element={
                   <div className="flex flex-col gap-3">
                     <span className="px-3">
-                      <CreateTaskModal />
+                      <Modal
+                        title="Create Task"
+                        content={
+                          <form className="h-max py-4 flex flex-col gap-3">
+                            <input
+                              name="task-name"
+                              type="text"
+                              placeholder="Type here"
+                              className="input input-bordered w-full "
+                            />
+                            <textarea
+                              className="textarea textarea-bordered h-24"
+                              placeholder="Description"
+                              name="task-description"
+                            />
+
+                            <select className="select select-bordered">
+                              <option disabled selected>
+                                Project
+                              </option>
+                              <option>Star Wars</option>
+                              <option>Harry Potter</option>
+                              <option>Lord of the Rings</option>
+                              <option>Planet of the Apes</option>
+                              <option>Star Trek</option>
+                            </select>
+
+                            <select className="select select-bordered">
+                              <option disabled selected>
+                                Priority
+                              </option>
+                              <option>Low</option>
+                              <option>Medium</option>
+                              <option>High</option>
+                              <option>Critical</option>
+                            </select>
+
+                            <input
+                              type="date"
+                              className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                              placeholder="Select a date"
+                            />
+
+                            <button type="button" className="btn">
+                              Create Task
+                            </button>
+                          </form>
+                        }
+                      />
                     </span>
                     <Table
                       headerArr={[
