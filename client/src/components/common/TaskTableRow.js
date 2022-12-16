c8897cab290943f722c4aeb92fee5072690bace1
arkii1 <tommy.mapp@hotmail.com>
2022-12-16 15:58:05 +0000

    Add TaskTableRow

---
+++ client/src/components/common/TaskTableRow.js
@@ -0,0 +1,96 @@
+import React, { useMemo } from 'react'
+import propTypes from 'prop-types'
+import uniqid from 'uniqid'
+
+import Modal from './Modal'
+import TaskForm from './TaskForm'
+import Checkbox from './Checkbox'
+
+function TaskTableRow({
+  index,
+  id,
+  name,
+  project,
+  description,
+  due,
+  priority,
+  createdAt,
+  completed = false,
+}) {
+  const key = useMemo(() => uniqid(), [])
+
+  return (
+    <tr key={key}>
+      <td>{index}</td>
+      <td>{name}</td>
+      <td>{project}</td>
+      <td>
+        <Modal
+          title="Details"
+          content={
+            <TaskForm
+              active={false}
+              taskData={
+                (id,
+                name,
+                description,
+                project,
+                priority,
+                createdAt,
+                due,
+                completed)
+              }
+            />
+          }
+        />
+      </td>
+      <td>{priority}</td>
+      <td>{due}</td>
+      <td>{createdAt}</td>
+      <td>
+        <Checkbox forName={key} startCompleted={completed} />
+      </td>
+      <td>
+        {' '}
+        <Modal
+          title="Details"
+          content={
+            <TaskForm
+              active
+              taskData={
+                (id,
+                name,
+                description,
+                project,
+                priority,
+                createdAt,
+                due,
+                completed)
+              }
+            />
+          }
+        />
+      </td>
+      <td>
+        <Modal
+          title="Delete"
+          content={<div>Are you sure you want to delete this task?</div>}
+        />
+      </td>
+    </tr>
+  )
+}
+
+export default TaskTableRow
+
+TaskTableRow.propTypes = {
+  index: propTypes.number.isRequired,
+  id: propTypes.string.isRequired,
+  name: propTypes.string.isRequired,
+  description: propTypes.string.isRequired,
+  project: propTypes.string.isRequired,
+  priority: propTypes.string.isRequired,
+  due: propTypes.string.isRequired,
+  createdAt: propTypes.string.isRequired,
+  completed: propTypes.bool,
+}
