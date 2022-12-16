67d59e538f7013fc3acbff207597d93545b14da0
arkii1 <tommy.mapp@hotmail.com>
2022-12-16 15:58:32 +0000

    Modify how rows are made

--- client/src/components/common/Table.js
+++ client/src/components/common/Table.js
@@ -2,17 +2,9 @@ import React from 'react'
 import propTypes from 'prop-types'
 import uniqid from 'uniqid'
 
-function Table({ headerArr, dataArr }) {
+function Table({ headerArr, rows }) {
   const headers = headerArr.map((h) => <th key={uniqid()}>{h}</th>)
 
-  const rows = dataArr.map((data, index) => (
-    <tr key={uniqid()}>
-      <td>{index + 1}</td>
-      {data.map((d) => (
-        <td key={uniqid()}>{d}</td>
-      ))}
-    </tr>
-  ))
   return (
     <div className="overflow-x-auto w-full px-3">
       <table className="table table-zebra w-full">
@@ -32,7 +24,5 @@ export default Table
 
 Table.propTypes = {
   headerArr: propTypes.arrayOf(propTypes.string).isRequired,
-  dataArr: propTypes.arrayOf(
-    propTypes.arrayOf(propTypes.oneOfType([propTypes.string, propTypes.object]))
-  ).isRequired,
+  rows: propTypes.arrayOf(propTypes.node).isRequired,
 }
