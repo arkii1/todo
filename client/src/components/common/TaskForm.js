f46005abe77cbae95496de4f66f1375b760f6416
arkii1 <tommy.mapp@hotmail.com>
2022-12-16 16:20:32 +0000

    Make input use state values

--- client/src/components/common/TaskForm.js
+++ client/src/components/common/TaskForm.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import propTypes from 'prop-types'
 
 function TaskForm({ active = true, taskData = {} }) {
+  const [name, setName] = useState(taskData.name || '')
+  const [description, setDescription] = useState(taskData.description || '')
+  const [project, setProject] = useState(taskData.project || 'Project')
+  const [priority, setPriority] = useState(taskData.priority || '')
+  const [due, setDue] = useState(taskData.due || '')
+
+  const handleNameChange = (e) => setName(e.target.value)
+  const handleDescriptionChange = (e) => setDescription(e.target.value)
+  const handleProjectChange = (e) => setProject(e.target.value)
+  const handlePriorityChange = (e) => setPriority(e.target.value)
+
+  const handleDueChange = (e) => setDue(e.target.value)
+
   return (
     <form className="h-max py-4 flex flex-col gap-3">
       <input
@@ -9,18 +22,25 @@ function TaskForm({ active = true, taskData = {} }) {
         type="text"
         placeholder="Type here"
         className="input input-bordered w-full"
-        value={taskData.name || ''}
+        value={name}
         disabled={!active}
+        onChange={handleNameChange}
       />
       <textarea
         className="textarea textarea-bordered h-24"
         placeholder="Description"
         name="task-description"
-        value={taskData.description || ''}
+        value={description}
         disabled={!active}
+        onChange={handleDescriptionChange}
       />
 
-      <select disabled={!active} className="select select-bordered">
+      <select
+        disabled={!active}
+        className="select select-bordered"
+        value={project}
+        onChange={handleProjectChange}
+      >
         <option disabled selected>
           {taskData.project || 'Project'}
         </option>
@@ -31,7 +51,12 @@ function TaskForm({ active = true, taskData = {} }) {
         <option>Star Trek</option>
       </select>
 
-      <select disabled={!active} className="select select-bordered">
+      <select
+        disabled={!active}
+        className="select select-bordered"
+        value={priority}
+        onChange={handlePriorityChange}
+      >
         <option disabled selected>
           {taskData.priority || 'Priority'}
         </option>
@@ -45,8 +70,9 @@ function TaskForm({ active = true, taskData = {} }) {
         type="date"
         className="block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
         placeholder="Select a date"
-        value={taskData.due || ''}
+        value={due}
         disabled={!active}
+        onChange={handleDueChange}
       />
       <input
         type="checkbox"
