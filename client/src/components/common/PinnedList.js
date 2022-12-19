874d09f07d9b1f892f883cd6d0125512e6bbdaf7
arkii1 <tommy.mapp@hotmail.com>
2022-12-19 16:24:42 +0000

    Change header font weight

--- client/src/components/common/PinnedList.js
+++ client/src/components/common/PinnedList.js
@@ -7,7 +7,7 @@ import './styles.css'
 function PinnedList({ title, dataArr }) {
   return (
     <div className="pinned-list flex flex-col justify-around px-3 pt-0 pb-3 gap-2">
-      <h3 className="text-lg font-medium text-gray-500	">{title}</h3>
+      <h3 className="text-lg font-normal text-gray-500	">{title}</h3>
       <div className="flex justify-between">
         {dataArr.map((data) => (
           <PinnedCard
