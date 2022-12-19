8ff7f64c7514a35e0f785b88d70f27c6221054df
arkii1 <tommy.mapp@hotmail.com>
2022-12-19 16:20:31 +0000

    Add fancy name section

--- client/src/components/common/ProjectTableRow.js
+++ client/src/components/common/ProjectTableRow.js
@@ -3,14 +3,27 @@ import propTypes from 'prop-types'
 import uniqid from 'uniqid'
 import { Link } from 'react-router-dom'
 
-function ProjectTableRow({ index, name, lastUpdated, link }) {
+function ProjectTableRow({ colour, name, team, lastUpdated, link }) {
   const key = useMemo(() => uniqid(), [])
+
+  const data = (
+    <div className="flex items-center justify-start gap-3">
+      <span
+        className="rounded-full overflow-hidden h-2 w-2"
+        // For some reason, `bg-${colour}` isn't working, so can't pass colour as "red" and instead need to specify colour id
+        style={{ backgroundColor: colour }}
+      />
+      <div className="flex justify-start items-baseline gap-1">
+        <h4>{name} </h4>
+        <small style={{ color: 'rgba(1,1,1,0.4)' }}>in {team}</small>
+      </div>
+    </div>
+  )
 
   return (
     <tr key={key}>
-      <td>{index}</td>
       <td>
-        <Link to={link}>{name}</Link>
+        <Link to={link}>{data}</Link>
       </td>
       <td>{lastUpdated}</td>
     </tr>
@@ -20,8 +33,9 @@ function ProjectTableRow({ index, name, lastUpdated, link }) {
 export default ProjectTableRow
 
 ProjectTableRow.propTypes = {
-  index: propTypes.number.isRequired,
-  name: propTypes.string.isRequired,
+  colour: propTypes.string.isRequired,
+  name: propTypes.node.isRequired,
+  team: propTypes.string.isRequired,
   lastUpdated: propTypes.string.isRequired,
   link: propTypes.string.isRequired,
 }
