4b4e51dd644308f0a39e0591cdef9d6a2fd48d00
arkii1 <tommy.mapp@hotmail.com>
2022-12-19 16:47:55 +0000

    Modify padding

--- client/src/components/common/Modal.js
+++ client/src/components/common/Modal.js
@@ -20,10 +20,10 @@ function Modal({ title, content }) {
         <div className="fixed w-screen h-screen z-50 bg-gray-700/75 inset-0 flex justify-center	">
           <div
             ref={modalRef}
-            className="fixed p-3 z-50 bg-white rounded-lg h-max top-8 sm:top-12 md:top-20 "
+            className="fixed p-3 z-50 bg-white rounded-lg h-max top-8 sm:top-12 md:top-20 p-3"
             style={{ maxWidth: '80%' }}
           >
-            <h3 className="px-1">{title}</h3>
+            <h3>{title}</h3>
             {content}
           </div>
         </div>
